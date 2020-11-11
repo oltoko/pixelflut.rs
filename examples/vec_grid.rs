f26df3b5a859840e0b1701c838fb4f6012ede1e7
Oliver Koch <github@oltoko.de>
2020-11-12 00:02:33 +0100

    Added PX <x> <y> as command
    Also added more documentation and changed u32 to usize

---
+++ examples/vec_grid.rs
@@ -0,0 +1,59 @@
+use simple_logger::SimpleLogger;
+
+use pixelflut_rs::grid::{Grid, Size};
+use pixelflut_rs::pixel::{Color, Coordinate, Pixel};
+use pixelflut_rs::server::Server;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    SimpleLogger::new().init().unwrap();
+
+    let grid = VecGrid::new(Size::new(1024, 768));
+
+    let server = Server::new("0.0.0.0".parse()?, 2342, grid);
+    server.start().await
+}
+
+struct VecGrid {
+    size: Size,
+    frame: Vec<Vec<Color>>,
+}
+
+impl VecGrid {
+    pub fn new(size: Size) -> VecGrid {
+        let black = Color::rgb(0x00, 0x00, 0x00);
+        let frame = vec![vec![black; size.y()]; size.x()];
+        VecGrid {
+            size,
+            frame,
+        }
+    }
+}
+
+impl Grid for VecGrid {
+    fn size(&self) -> Size {
+        self.size.clone()
+    }
+
+    fn draw(&mut self, px: Pixel) {
+        let x = px.coordinate().x();
+        let y = px.coordinate().y();
+
+        if x < self.size.x() && y < self.size.y() {
+            self.frame[x][y] = px.color();
+        }
+    }
+
+    fn fetch(&self, coord: Coordinate) -> Option<Pixel> {
+        let x = coord.x();
+        let y = coord.y();
+
+        if x < self.size.x() && y < self.size.y() {
+            let color = self.frame[x][y];
+            return Some(Pixel::new(coord, color))
+        }
+
+        None
+    }
+}
+
--- src/server.rs
+++ src/server.rs
@@ -10,10 +10,12 @@ use tokio::net::{TcpListener, TcpStream};
 use tokio::task;
 
 use crate::grid::{Grid, Size};
+use crate::pixel::Pixel;
 
 const HELP: &str = "\
 HELP Pixelflut Commands:\n\
 HELP - PX <x> <y> <RRGGBB[AA]>\n\
+HELP - PX <x> <y>   >>  PX <x> <y> <RRGGBB>\n\
 HELP - SIZE         >>  SIZE <width> <height>\n\
 HELP - HELP         >>  HELP ...\n";
 
@@ -21,6 +23,16 @@ custom_error! { ServerError
     UnknownCommand = "Unknown command send!"
 }
 
+/// The Pixelflut Server.
+///
+/// The Server is defined by an interface and a port where it should listen on. It
+/// also requires a Grid on which the Pixels should be drawn on. To start everything you just
+/// need to do the following:
+///
+/// ```compile_fail
+/// let server = Server::new("0.0.0.0".parse()?, 2342, grid);
+/// server.start().await
+/// ```
 pub struct Server<G: Grid + std::marker::Send> {
     interface: IpAddr,
     port: u16,
@@ -31,6 +43,7 @@ impl<G> Server<G>
     where
         G: 'static + Grid + std::marker::Send,
 {
+    /// Creates a new Server which with the given interface, port and Grid.
     pub fn new(interface: IpAddr, port: u16, grid: G) -> Server<G> {
         Server {
             interface,
@@ -39,7 +52,7 @@ impl<G> Server<G>
         }
     }
 
-    /// This method never returns.
+    /// This method will start your server and will never return without an error.
     pub async fn start(self) -> Result<(), Box<dyn std::error::Error>> {
         // Bind the listener to the address
         let listener = TcpListener::bind((self.interface, self.port)).await?;
@@ -76,9 +89,26 @@ async fn process<G: Grid>(
         let mut parts = line.split_whitespace();
         match parts.next() {
             Some("PX") => {
-                let grid = grid.lock().unwrap();
-                grid.draw(line.parse()?)
-            },
+                match parts.count() {
+                    // PX <x> <y>
+                    2 => {
+                        let pixel: Option<Pixel>;
+                        {
+                            let grid = grid.lock().unwrap();
+                            pixel = grid.fetch(line.parse()?);
+                        }
+                        if pixel.is_some() {
+                            wr.write(pixel.unwrap().to_string().as_bytes()).await?;
+                        }
+                    }
+                    // PX <x> <y> <RRGGBB[AA]>
+                    3 => {
+                        let mut grid = grid.lock().unwrap();
+                        grid.draw(line.parse()?)
+                    }
+                    _ => return Err(Box::new(ServerError::UnknownCommand)),
+                }
+            }
             Some("SIZE") => {
                 let size;
                 {
@@ -86,7 +116,7 @@ async fn process<G: Grid>(
                     size = grid.size().to_string();
                 }
                 wr.write(size.as_bytes()).await?;
-            },
+            }
             Some("HELP") => { wr.write(HELP.as_bytes()).await?; }
             _ => return Err(Box::new(ServerError::UnknownCommand)),
         }
@@ -97,8 +127,7 @@ async fn process<G: Grid>(
 
 impl fmt::Display for Size {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        let xy = self.xy();
-        writeln!(f, "SIZE {} {}", xy.0, xy.1)
+        writeln!(f, "SIZE {} {}", self.x(), self.y())
     }
 }
 
